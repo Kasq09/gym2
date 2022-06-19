d67ec2b806561508459a9a133b6609ff891ef39d
Lapegle <Daniels.Gabranovs@outlook.lv>
2022-06-19 22:32:24 +0300

    update database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'admin@example.com',
+            'name' => 'Kaspars Rudzusieks',
+            'email' => 'kaspars@gmail.com',
+            'role' => 'admin'
         ]);
     }
 }
